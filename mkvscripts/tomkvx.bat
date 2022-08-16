603c7544b90839878e3443e731773f2ed8ecf7fc
kz.dev370 <kz.dev370@gmail.com>
2022-08-16 02:24:25 +0200

    update script

--- mkvscripts/tomkvx.bat
+++ mkvscripts/tomkvx.bat
@@ -5,20 +5,24 @@ set /p mkvDir= mkv dir:
 
 if not DEFINED "%mkDir%" (
     set mkvtoolnix="C:\Program Files\MKVToolNix\mkvmerge.exe"  
-)ELSE (
+) ELSE (
     set mkvtoolnix="C:\Program Files\MKVToolNix\%mkvDir%\mkvmerge.exe"
 )
 
 if not exist %mkvtoolnix% ( exit )
 
 for /d %%x in ("%CD%\*") do (
-if not exist "%CD%\mkvmerge_old" (mkdir "%CD%\mkvmerge_old")
-for %%A in ("%%x\*.%extention%") do (
-if not exist "%%x\%%~nA.mkv" (
-move "%%x\%%~nA.%extention%" "%%x\mkvmerge_old\%%~nA.%extention%"
-%mkvtoolnix% --output "%%x/%%~nA.mkv" "%%x\mkvmerge_old\%%~nA.%extention%" 
+    if not exist "%CD%\mkvmerge_old" (mkdir "%CD%\mkvmerge_old")
+    for %%A in ("%%x\*.%extention%") do (
+        if not exist "%%x\%%~nA.mkv" (
+            move "%%x\%%~nA.%extention%" "%%x\mkvmerge_old\%%~nA.%extention%"
+        ) else (
+            move "%%x\%%~nA.%extention%" "%%x\mkvmerge_old\%%~nA.%extention%"
+            move "%%x\%%~nA.mkv" "%%x\mkvmerge_old\%%~nA.mkv"
+        )
+        %mkvtoolnix% --output "%%x/%%~nA.mkv" "%%x\mkvmerge_old\%%~nA.%extention%" 
+    )
 )
-))
 echo.
 echo ============================
 echo Done
