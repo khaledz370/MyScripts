4e099f42ecf481a500f3954c9f6867525aed7e69
kz370 <kz.dev370@gmail.com>
2023-09-17 13:48:14 +0300

    one piece arc scripts

--- mkvscripts/mkvOnePiece.bat
+++ mkvscripts/mkvOnePiece.bat
@@ -7,11 +7,11 @@ if not exist %mkvtoolnix% ( exit )
 
 if not exist "%CD%\mkvmerge_old" (mkdir "%CD%\mkvmerge_old")
 for %%A in ("%CD%\*.mkv") do (
-    if exist "%CD%\%%~nA.mkv" if exist "%CD%\%%~nA.en.srt" if exist "%CD%\%%~nA.en.srt" (
+    if exist "%CD%\%%~nA.mkv" if exist "%CD%\%%~nA.de.srt" if exist "%CD%\%%~nA.en.srt" (
             move "%CD%\%%~nA.mkv" "%CD%/mkvmerge_old/%%~nA.mkv"
-            move "%CD%\%%~nA.srt"  "%CD%/mkvmerge_old/%%~nA.srt" 
+            move "%CD%\%%~nA.de.srt"  "%CD%/mkvmerge_old/%%~nA.de.srt" 
             move "%CD%\%%~nA.en.srt"  "%CD%/mkvmerge_old/%%~nA.en.srt" 
-            %mkvtoolnix% --output "%CD%/%%~nA.mkv" "%CD%/mkvmerge_old/%%~nA.mkv" --language 0:de "%CD%/mkvmerge_old/%%~nA.srt" --language 0:en --default-track-flag 0:no "%CD%/mkvmerge_old/%%~nA.en.srt" 
+            %mkvtoolnix% --output "%CD%/%%~nA.mkv" "%CD%/mkvmerge_old/%%~nA.mkv" --language 0:de "%CD%/mkvmerge_old/%%~nA.de.srt" --language 0:en --default-track-flag 0:no "%CD%/mkvmerge_old/%%~nA.en.srt" 
 
             Set "File=%%~nxA"
             Ren "%%A" "!File: no sub=!"
---
+++ translate.bat
@@ -0,0 +1,3 @@
+set /p file=subtitle file: 
+translatesubs %file% "G:\Downloads\out.srt" --to_lang ar
+@pause
